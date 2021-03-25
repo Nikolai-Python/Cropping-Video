91830229bcdfb71bd3901516f12be572722e3002
Nikolai <n.v.eremenko@gmail.com>
2021-03-25 19:50:53 +0400

    Initial commit

---
+++ camweb.py
@@ -0,0 +1,11 @@
+import cv2
+vidcap = cv2.VideoCapture('Прорыв Феттеля с последнего до 4-го места Малайзия - 2017.mp4')
+success, image = vidcap.read()
+count = 0
+success = True
+while success:
+    vidcap.set(cv2.CAP_PROP_POS_MSEC,(count*15000))
+    cv2.imwrite("jpeg\\frame%d.jpg" % count, image) 
+    success, image = vidcap.read()
+    print('Read a new frame: ', success)
+    count += 1
